326282669b3790eecb70eaf013d2ed78d2c1b8d5
Halid <denievhalid88@mail.ru>
2022-08-10 15:36:09 +0300

    commit

--- src/api.js
+++ src/api.js
@@ -5,18 +5,42 @@ const api = axios.create({
 });
 
 class User {
+  /**
+   * Метод для получения списка всех пользователей
+   *
+   * @param page
+   * @returns {Promise<AxiosResponse<any>>}
+   */
   static getList(page = 1) {
     return api.get(`/users/?page=${page}`);
   }
 
+  /**
+   * Метод для получения пользователя по ID
+   *
+   * @param id
+   * @returns {Promise<AxiosResponse<any>>}
+   */
   static getById(id) {
     return api.get(`/users/${id}`).then(resolveResponse);
   }
 
+  /**
+   * Метод для создания пользователя
+   *
+   * @param credentials
+   * @returns {Promise<AxiosResponse<any>>}
+   */
   static create(credentials) {
     return api.post(`/users`, credentials).then(resolveResponse);
   }
 
+  /**
+   * Метод для удаления пользователя
+   *
+   * @param id
+   * @returns {Promise<AxiosResponse<any>>}
+   */
   static delete(id) {
     return api.delete(`/users/${id}`);
   }
